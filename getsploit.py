a549082a3b5f30f1d8a8b8ffbd5325ef13ffce9c
Eugene Russkikh <zitryss@gmail.com>
2017-07-21 20:52:58 +0200

    Rename variables

--- getsploit.py
+++ getsploit.py
@@ -44,37 +44,37 @@ def main():
     parser.add_argument("-c", "--count", default=10, type=int, help="Search limit. Default 10.")
     args = parser.parse_args()
     search_results = search_exploit(args)
-    outputTable = texttable.Texttable()
-    outputTable.set_cols_dtype(['t', 't', 't'])
-    outputTable.set_cols_align(['c', 'l', 'c'])
-    outputTable.set_cols_width(['20', '30', '100'])
-    tableRows = [['ID', 'Exploit Title', 'URL']]
-    jsonRows = []
+    output_table = texttable.Texttable()
+    output_table.set_cols_dtype(['t', 't', 't'])
+    output_table.set_cols_align(['c', 'l', 'c'])
+    output_table.set_cols_width(['20', '30', '100'])
+    table_rows = [['ID', 'Exploit Title', 'URL']]
+    json_rows = []
     for bulletinSource in search_results.get("search"):
         bulletin = bulletinSource.get('_source')
-        bulletinUrl = bulletin.get('vref') or 'https://vulners.com/%s/%s' % (bulletin.get('type'), bulletin.get('id'))
-        tableRows.append([bulletin.get('id'), bulletin.get('title'), bulletinUrl])
+        bulletin_url = bulletin.get('vref') or 'https://vulners.com/%s/%s' % (bulletin.get('type'), bulletin.get('id'))
+        table_rows.append([bulletin.get('id'), bulletin.get('title'), bulletin_url])
         if args.json:
-            jsonRows.append({'id':bulletin.get('id'), 'title':bulletin.get('title'), 'url':bulletinUrl})
+            json_rows.append({'id': bulletin.get('id'), 'title': bulletin.get('title'), 'url': bulletin_url})
         if args.mirror:
-            pathName = './%s' % normalize_string(args.query)
+            path_name = './%s' % normalize_string(args.query)
             # Put results it the dir
-            if not os.path.exists(pathName):
-                os.mkdir(pathName)
-            with open("./%s/%s.txt" % (pathName, normalize_string(bulletin.get('id'))), 'w') as exploitFile:
-                exploitData = bulletin.get('sourceData') or bulletin.get('description')
-                exploitFile.write(exploitData)
+            if not os.path.exists(path_name):
+                os.mkdir(path_name)
+            with open("./%s/%s.txt" % (path_name, normalize_string(bulletin.get('id'))), 'w') as exploitFile:
+                exploit_data = bulletin.get('sourceData') or bulletin.get('description')
+                exploitFile.write(exploit_data)
     if args.json:
         # Json output
-        print(json.dumps(jsonRows))
+        print(json.dumps(json_rows))
     else:
         # Text output
         print("Total found exploits: %s" % search_results.get('total'))
         # Set max coll width by len of the url for better copypaste
-        maxWidth = max(len(element[2]) for element in tableRows)
-        outputTable.set_cols_width([20, 30, maxWidth])
-        outputTable.add_rows(tableRows)
-        print(outputTable.draw())
+        max_width = max(len(element[2]) for element in table_rows)
+        output_table.set_cols_width([20, 30, max_width])
+        output_table.add_rows(table_rows)
+        print(output_table.draw())
 
 
 if __name__ == '__main__':
