a189ffaffa26e7a6adb413b7cbd81c7d93f17371
Eugene Russkikh <zitryss@gmail.com>
2017-07-21 19:59:19 +0200

    Remove unnecessary functionality

--- getsploit.py
+++ getsploit.py
@@ -5,22 +5,11 @@ import json
 import os
 import os.path
 import re
-import sqlite3
 import ssl
-import sys
 import urllib.parse
 import urllib.request
-import zipfile
 
 import texttable
-
-VULNERS_URL = {
-    'searchAPI': 'https://vulners.com/api/v3/search/lucene/',
-    'updateAPI': 'https://vulners.com/api/v3/archive/getsploit/',
-    'idAPI': 'https://vulners.com/api/v3/search/id/',
-}
-DBPATH, SCRIPTNAME = os.path.split(os.path.abspath(__file__))
-DBFILE = os.path.join(DBPATH, 'getsploit.db')
 
 
 def slugify(value):
@@ -31,38 +20,6 @@ def slugify(value):
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     value = re.sub('[-\s]+', '-', value)
     return value
-
-
-def progress_callback_simple(downloaded, total):
-    sys.stdout.write(
-        "\r" +
-        (len(str(total)) - len(str(downloaded))) * " " + str(downloaded) + "/%d" % total +
-        " [%3.2f%%]" % (100.0 * float(downloaded) / float(total))
-    )
-    sys.stdout.flush()
-
-
-def downloadFile(srcurl, dstfilepath, progress_callback=None, block_size=8192):
-    def _download_helper(response, out_file, file_size):
-        if progress_callback!=None: progress_callback(0,file_size)
-        if block_size == None:
-            buffer = response.read()
-            out_file.write(buffer)
-            if progress_callback!=None: progress_callback(file_size,file_size)
-        else:
-            file_size_dl = 0
-            while True:
-                buffer = response.read(block_size)
-                if not buffer: break
-                file_size_dl += len(buffer)
-                out_file.write(buffer)
-                if progress_callback!=None: progress_callback(file_size_dl,file_size)
-    with open(dstfilepath,"wb") as out_file:
-        opener = getUrllibOpener()
-        req = urllib.request.Request(srcurl)
-        with opener.open(req) as response:
-            file_size = int(response.getheader("Content-Length"))
-            _download_helper(response,out_file,file_size)
 
 
 def getUrllibOpener():
@@ -76,25 +33,13 @@ def getUrllibOpener():
 
 def searchVulnersQuery(searchQuery, limit):
     vulnersSearchRequest = {"query":searchQuery, 'skip':0, 'size':limit}
-    req = urllib.request.Request(VULNERS_URL['searchAPI'])
+    req = urllib.request.Request("https://vulners.com/api/v3/search/lucene/")
     response = getUrllibOpener().open(req, json.dumps(vulnersSearchRequest).encode('utf-8'))
     responseData = response.read()
     if isinstance(responseData, bytes):
         responseData = responseData.decode('utf8')
     responseData = json.loads(responseData)
     return responseData
-
-
-def downloadVulnersGetsploitDB(path):
-    archiveFileName = os.path.join(path, 'getsploit.db.zip')
-    print("Downloading getsploit database archive. Please wait, it may take time. Usually around 5-10 minutes.")
-    downloadFile(VULNERS_URL['updateAPI'], archiveFileName, progress_callback=progress_callback_simple)
-    print("\nUnpacking database.")
-    zip_ref = zipfile.ZipFile(archiveFileName, 'r')
-    zip_ref.extractall(DBPATH)
-    zip_ref.close()
-    os.remove(archiveFileName)
-    return True
 
 
 def exploitSearch(query, lookupFields=None, limit=10):
@@ -107,37 +52,6 @@ def exploitSearch(query, lookupFields=None, limit=10):
     return searchQuery, searchResults
 
 
-def exploitLocalSearch(query, lookupFields=None, limit=10):
-    # Build query
-    # CREATE VIRTUAL TABLE exploits USING FTS4(id text, title text, published DATE, description text, sourceData text, vhref text)
-    sqliteConnection = sqlite3.connect(DBFILE)
-    cursor = sqliteConnection.cursor()
-    # Check if FTS4 is supported
-    ftsok = False
-    for (val,) in cursor.execute('pragma compile_options'):
-        if ('FTS4' in val) or ('FTS3' in val):
-            ftsok = True
-    if not ftsok:
-        print("Your SQLite3 library does not support FTS4. Sorry, without this option local search will not work. Recompile SQLite3 with ENABLE_FTS4 option.")
-        exit()
-    preparedQuery = " AND ".join(['"%s"' % word for word in query.split()])
-    searchRawResults = cursor.execute("SELECT * FROM exploits WHERE exploits MATCH ? ORDER BY published LIMIT ?", ('%s' % preparedQuery,limit)).fetchall()
-    searchCount = cursor.execute("SELECT Count(*) FROM exploits WHERE exploits MATCH ? ORDER BY published LIMIT ?", ('%s' % preparedQuery,limit)).fetchone()
-    searchResults = {'total':searchCount,'search':[]}
-    for element in searchRawResults:
-        searchResults['search'].append({'_source':
-                                               {'id':element[0],
-                                                'title':element[1],
-                                                'published':element[2],
-                                                'description':element[3],
-                                                'sourceData':element[4],
-                                                'vhref':element[5],
-                                                }
-                                        })
-    # Output must b
-    return query, searchResults
-
-
 def main():
     parser = argparse.ArgumentParser(description="Exploit search and download utility")
     parser.add_argument("query", type=str, help="Exploit search query. See https://vulners.com/help for the detailed manual.")
@@ -145,23 +59,8 @@ def main():
     parser.add_argument("-j", "--json", action="store_true", help="Show result in JSON format.")
     parser.add_argument("-m", "--mirror", action="store_true", help="Mirror (aka copies) search result exploit files to the subdirectory with your search query name.")
     parser.add_argument("-c", "--count", default=10, type=int, help="Search limit. Default 10.")
-    parser.add_argument("-l", "--local", action="store_true", help="Perform search in the local database instead of searching online.")
-    parser.add_argument("-u", "--update", action="store_true", help="Update getsploit.db database. Will be downloaded in the script path.")
     args = parser.parse_args()
-
-    if args.update:
-        downloadVulnersGetsploitDB(DBPATH)
-        print("Database download complete. Now you may search exploits using --local key './getsploit.py -l wordpress 4.7'")
-        exit()
-
-    if args.local:
-        if not os.path.exists(DBFILE):
-            print("There is no local database file near getsploit. Run './getsploit.py --update'")
-            exit()
-        finalQuery, searchResults = exploitLocalSearch(args.query, lookupFields=['title'] if args.title else None, limit = args.count)
-    else:
-        finalQuery, searchResults = exploitSearch(args.query, lookupFields=['title'] if args.title else None, limit = args.count)
-
+    finalQuery, searchResults = exploitSearch(args.query, lookupFields=['title'] if args.title else None, limit = args.count)
     outputTable = texttable.Texttable()
     outputTable.set_cols_dtype(['t', 't', 't'])
     outputTable.set_cols_align(['c', 'l', 'c'])
@@ -182,7 +81,6 @@ def main():
             with open("./%s/%s.txt" % (pathName,slugify(bulletin.get('id'))), 'w') as exploitFile:
                 exploitData = bulletin.get('sourceData') or bulletin.get('description')
                 exploitFile.write(exploitData)
-
     if args.json:
         # Json output
         print(json.dumps(jsonRows))
